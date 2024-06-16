b8877cb3319f19568603f39798cd50b01ee21b31
Daniel Ogundiran <56872396+theakanfecode1@users.noreply.github.com>
2024-06-16 21:02:19 +0100

    Update ActionDriver.java

--- src/ActionDriver.java
+++ src/ActionDriver.java
@@ -2,19 +2,13 @@ public class ActionDriver {
 
 
     public static void main(String[] args) {
-
-
-
         System.out.println("RegularAction is just a subclass of Action: "+ (Action.class.isAssignableFrom(RegularAction.class)));
         System.out.println("RegularAction activities have no extra fields: "+ (RegularAction.class.getDeclaredFields().length == 0 ));
-
         RegularAction regularAction = new RegularAction();
         regularAction.setDescription("Wash your hands");
         System.out.println("Looking at regular actions: "+ regularAction.toString());
         System.out.println(regularAction.occursOn(2022,3,15));
-
-
-    }
+  }
 
 
 }
